f110de0c85badba234f50980298e269b06d3b68b
sanyas-dada <sanyasdada09@gmail.com>
2022-04-11 15:56:32 +0545

    second commit

--- app/src/main/java/com/sanyasdada/gitpractiseexample/MainActivity.java
+++ app/src/main/java/com/sanyasdada/gitpractiseexample/MainActivity.java
@@ -1,5 +1,7 @@
 package com.sanyasdada.gitpractiseexample;
 
+import static java.sql.DriverManager.println;
+
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
@@ -10,5 +12,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        println("i am going to learn git today");
     }
 }
